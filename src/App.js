e0fdaa0b7dafd07e460f6d20c966a0d2a0c1f613
jknadan@gmail.com <wnsrl91828@>
2021-01-15 17:56:13 +0900

    #1_jknadan_theory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function formatInfo(student){
+  return student.name + "[" + student.id + "]"
+}
+
+const student = {
+  id: "2017707079",
+  name: "조준기"
+}
+
+const element = (
+    <h2>
+      {formatInfo(student)}
+    </h2>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return element
+
 }
 
 export default App;
